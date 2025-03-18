4d74a8450f163b1380bd5cba57fdb1af13de3393
Giannamia Gavilanes <719613@student.dusd.net>
2025-03-18 12:04:43 -0700

    Finished my Challenge 1
    I finished fixing all the bugs in my Challenge 1 assignment.

--- U1PlayerControlP4Gavilanes/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ U1PlayerControlP4Gavilanes/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(70, 0, 0);
+    private Vector3 offset = new Vector3(40f, 0, 3.9f);
 
     // Start is called before the first frame update
     void Start()
